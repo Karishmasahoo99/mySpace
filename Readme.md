88ae97a69fffb35fe5cb392447f1b6a6d997f63f
Karishma Sahoo <karishmasahoo30@gmail.com>
2024-06-14 18:08:57 +0530

    Utils updated

--- Readme.md
+++ Readme.md
@@ -1,3 +1,40 @@
 MERN project
 
 If we want to pass empty folder in git, we have to use .gitkeep.
+
+import express from "express";
+const app=express();
+
+;(
+    async()=>{
+        try{
+            await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+            app.on("error", (error)=>{
+                console.log("Error while connecting to database. ERR:", error)
+                throw error
+            })
+
+            app.listen(process.env.PORT, ()=>{
+                console.log("App is running in port ",process.env.PORT)
+            })
+        }catch(err){
+            console.error("ERROR:", err);
+            throw err
+        }
+    }
+)()
+
+This is basic code which is messy and hence we have db folder, app folders and others.
+
+In express, we mainly deal with request and response. Two main things in request is req.params and req.body.
+
+Earlier we needed bodyParser package but nowadays it is by default included in express
+
+We can extend the Error class and define our errors
+
+Responses and status code:
+Informational responses: 100-199
+Successful responses: 200-299
+Redirection responses: 300-399
+Client error responses: 400-499
+Server error responses: 500-599
--- src/index.js
+++ src/index.js
@@ -1,34 +1,23 @@
 //require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
-
-
-// import express from "express";
-// const app=express();
-
-// ;(
-//     async()=>{
-//         try{
-//             await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//             app.on("error", (error)=>{
-//                 console.log("Error while connecting to database. ERR:", error)
-//                 throw error
-//             })
-
-//             app.listen(process.env.PORT, ()=>{
-//                 console.log("App is running in port ",process.env.PORT)
-//             })
-//         }catch(err){
-//             console.error("ERROR:", err);
-//             throw err
-//         }
-//     }
-// )()
-
-// This is basic code
+.then(()=>{
+    app.on(error,()=>{
+        console.log("Error while connecting to database. ERR:", error)
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port ${process.env.PORT}`)
+    })
+    
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed !!! ", err);
+})
